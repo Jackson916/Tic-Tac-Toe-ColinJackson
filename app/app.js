c0e1b64949dc1fdacd98a4cfa75bca21ecfa725f
jackson916 <colingrey412@gmail.com>
2022-03-24 14:18:00 -0400

    Used require with the appropriate reference to use in the file.
    Attached the event listeners for the sign in, sign up and sign out.

--- app/app.js
+++ app/app.js
@@ -6,4 +6,6 @@ const authEvents = require('./auth/events.js')
 
 $(() => {
   $('#sign-up-form').on('submit', authEvents.onSignUp)
+  $('#sign-in-form').on('submit', authEvents.onSignIn)
+  $('#sign-out-form').on('submit', authEvents.onSignOut)
 })
