61c1842423fe9072b3c69af976103896383baa3e
jackson916 <colingrey412@gmail.com>
2022-03-24 14:21:21 -0400

    Used require with the appropriate reference to use the file.
    Created the function for sign in, sign up, and sign out.
    
    Added the Get form fields

--- app/auth/events.js
+++ app/auth/events.js
@@ -1,24 +1,47 @@
 'use strict'
+
 const authUi = require('./ui.js')
+
 const authApi = require('./api.js')
+
 const getFormFields = require('../../lib/get-form-fields.js')
 
 const onSignUp = function (event) {
   event.preventDefault()
   console.log('IS WORKING')
 
-  // get data from form
   const form = event.target
   const data = getFormFields(form)
   console.log(data)
 
   authApi
-
     .signUp(data)
     .then(() => authUi.onSignUpSuccess())
     .catch(() => authUi.onSignUpFailure())
 }
 
+const onSignIn = function (event) {
+  event.preventDefault()
+  console.log('IS WORKING')
+
+  const form = event.target
+  const data = getFormFields(form)
+  console.log(data)
+
+  authApi
+    .signIn(data)
+    .then((response) => authUi.onSignInSuccess(response))
+    .catch(() => authUi.onSignInFailure())
+}
+const onSignOut = function () {
+  authApi
+    .signOut()
+    .then(() => authUi.onSignOutSuccess())
+    .catch(() => authUi.onSignOutFailure())
+}
+
 module.exports = {
-  onSignUp
+  onSignUp,
+  onSignIn,
+  onSignOut
 }
