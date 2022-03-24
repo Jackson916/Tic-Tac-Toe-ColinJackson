964d42c60173ab0de35c22a33ddaa4ea60035b26
jackson916 <colingrey412@gmail.com>
2022-03-24 14:28:13 -0400

    Used ajax to request data from the given url.
    used require to with the appropriate refrence.
    
    using the ../store.js that holds the temp data

--- app/auth/api.js
+++ app/auth/api.js
@@ -1,14 +1,36 @@
 'use strict'
 
+const store = require('../store.js')
+
 const signUp = function (data) {
   console.log(data)
   return $.ajax({
     method: 'POST',
-    url: 'https://tic-tac-toe-api-production.herokuapp.com/sign-up',
+    url: 'https://tic-tac-toe-api-development.herokuapp.com/sign-up',
     data
   })
 }
 
+const signIn = function (data) {
+  console.log(data)
+  return $.ajax({
+    method: 'POST',
+    url: 'https://tic-tac-toe-api-development.herokuapp.com/sign-in',
+    data
+  })
+}
+const signOut = function () {
+  return $.ajax({
+    method: 'DELETE',
+    url: 'https://tic-tac-toe-api-development.herokuapp.com/sign-out',
+    headers: {
+      Authorization: 'Bearer ' + store.user.token
+    }
+  })
+}
+
 module.export = {
-  signUp
+  signUp,
+  signIn,
+  signOut
 }
