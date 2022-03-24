6dff0d1ea3e83b7db94cc4f8f43ed6d83cef9ecc
jackson916 <colingrey412@gmail.com>
2022-03-24 14:35:44 -0400

    Created the onSuccess and Failure for the sign in , up, and out. Added the .trigger to clear on onSuccess.

--- app/auth/ui.js
+++ app/auth/ui.js
@@ -9,7 +9,30 @@ const onSignUpSuccess = function () {
 const onSignUpFailure = function () {
   $('#auth-display').html('<p>Error while signing up</p>')
 }
+
+const onSignInSuccess = function () {
+  $('#auth-display').html('<p>User signed in successfully</p>')
+
+  $('form').trigger('reset')
+}
+
+const onSignInFailure = function () {
+  $('#auth-display').html('<p>Error while signing in</p>')
+}
+const onSignOutSuccess = function () {
+  $('#auth-display').html('<p>User signed out successfully</p>')
+
+  $('form').trigger('reset')
+}
+
+const onSignOutFailure = function () {
+  $('#auth-display').html('<p>Error while signing out</p>')
+}
 module.export = {
   onSignUpSuccess,
-  onSignUpFailure
+  onSignUpFailure,
+  onSignInSuccess,
+  onSignInFailure,
+  onSignOutSuccess,
+  onSignOutFailure
 }
